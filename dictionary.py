630fd482515f4e44f22e51a7fe7a08b2c501aa84
teckniec <1anthony1@gmail.com>
2020-04-11 20:26:25 -0700

    cleaned up output to make it more readable

--- dictionary.py
+++ dictionary.py
@@ -7,10 +7,17 @@ data = json.load(open("data.json"))
 
 # Search function
 def translate(w):
+    w = w.lower()
     if w in data:
         return data[w]
     elif len(get_close_matches(w, data.keys())) > 0:
-        return "Did you mean %s instead?" % get_close_matches(w, data.keys())[0]    
+        yn = input("Did you mean %s? Enter Y if yes, or N if no: " % get_close_matches(w, data.keys())[0])    
+        if yn == "Y":
+            return data[get_close_matches(w, data.keys())[0]]
+        elif yn =="N":
+            return "This word does not exist, please check your spelling"
+        else:
+            return "We did not understand your entry. Try again"    
     else:
         return "The word does not exist. Check your spelling"
 
@@ -18,4 +25,11 @@ def translate(w):
 # user Input
 word = input("Enter word: ")
 
-print(translate(word.lower()))
+# Output
+output = translate(word)
+
+if type(output) == list:
+    for item in output:
+        print(item)
+else:
+    print(output)
