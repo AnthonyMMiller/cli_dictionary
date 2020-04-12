aadc25ef9727f44db7bd53e121ed77c92831e8e7
teckniec <1anthony1@gmail.com>
2020-04-11 19:58:25 -0700

    added condition to search for similar words

--- dictionary.py
+++ dictionary.py
@@ -1,15 +1,21 @@
 import json
+from difflib import get_close_matches
 
+#Open JSON file
 data = json.load(open("data.json"))
 
 
+# Search function
 def translate(w):
     if w in data:
         return data[w]
+    elif len(get_close_matches(w, data.keys())) > 0:
+        return "Did you mean %s instead?" % get_close_matches(w, data.keys())[0]    
     else:
         return "The word does not exist. Check your spelling"
 
 
+# user Input
 word = input("Enter word: ")
 
 print(translate(word.lower()))
